1ba5d7be7a6a01283a0c3f6cc4df6416a26bb6cd
IuriiRiabeka <141550450+IuriiRiabeka@users.noreply.github.com>
2024-02-15 12:51:35 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+  #value = String;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value
+  };
+  padStart(str) {
+    this.#value = str.concat(this.#value);
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value.concat(str);
+    return this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value.concat(str);
+    return this.#value;
+  }
+
+
+
+}
+
+
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
